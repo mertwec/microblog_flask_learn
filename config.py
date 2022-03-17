0f5727b63a439988ec806c62bdda172d4326113b
kloom <pavelfesenko.work@gmail.com>
2022-03-17 13:19:25 +0200

    init, parts 1-7 (index, create user, login, logout, user_page)

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'mbapp.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
